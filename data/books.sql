f229969c2757aa85190420e7a430bfc13ed19d63
Matthew_Heyert <heyerthb@gmail.com>
2019-06-21 13:58:31 -0700

    Fighting with Images

--- data/books.sql
+++ data/books.sql
@@ -5,7 +5,7 @@ CREATE TABLE books (
   author VARCHAR (255),
   title VARCHAR(255),
   isbn VARCHAR (255),
-  image_URL VARCHAR (255),
+  image_url VARCHAR (255),
   bookshelf VARCHAR  (255),
   description TEXT
 );
